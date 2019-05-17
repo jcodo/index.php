b85b5fc5b478d993610a71c290772d7fe40c556d
jcodo <43956180+jcodo@users.noreply.github.com>
2019-05-17 14:53:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 
-echo 'Hello World';
+$url = "https://api.giphy.com/v1/gifs/random?api_key=UjlbFDHmpWqQnC2Kyqd7ylHQz7Le7Ejf";
+echo '<img src="' . json_decode(file_get_contents($url), true)['data']['image_original_url'] . '">';
